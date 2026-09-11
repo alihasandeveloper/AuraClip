9ce581dfda8c7bd70c181faf705e25800149ef1c
Ali Hasan <alihasandeveloper@gmail.com>
2026-09-11 14:29:52 +0600

    Initial Commit

---
+++ ClipboardManager/ClipboardManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardManagerApp.swift
+//  ClipboardManager
+//
+//  Created by BoomDevs on 11/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipboardManager
+//
+//  Created by BoomDevs on 11/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
